7171681db79ce95d918e097527be1267e712ed9a
omer.ozkan <omer.ozkan@argela.com.tr>
2017-08-16 22:57:19 +0300

    dist commit!

---
+++ client/dist/assets/config/settings.js
@@ -0,0 +1,24 @@
+window.prognet_config = {
+    "server": {
+        "url": "192.168.30.114",
+        "port": 7171
+    },
+    "secure_server": {
+        "url": "c1.milat.info",
+        "port": 48443
+    },
+    "version": {
+        "sprint": "Milat-UI-1.4.3-Develop",
+        "build": "b999",
+        "date": "2017-08-14T19:35:00.000Z"
+    },
+    "kibana": {
+        "url": null, //if null then server.url will be used
+        "port": 5601,
+        "path": "/"
+    },
+    "authorization": {
+        "mode": "Basic",
+        "key": "cHJvZ25ldDpwcm9nbmV0"
+    }
+}
