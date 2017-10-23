1f52760157c489e5f5e98bebff934a563b0c6ff0
omer.ozkan <omer.ozkan@argela.com.tr>
2017-10-24 01:46:19 +0300

    dist publish path changed

---
+++ client/dist/assets/config/settings.js
@@ -0,0 +1,24 @@
+window.prognet_config = {
+    "server": {
+        "url": "sdnproject.herokuapp.com",
+        "port": 5000
+    },
+    "secure_server": {
+        "url": "sdnproject.herokuapp.com",
+        "port": 5000
+    },
+    "version": {
+        "sprint": "Milat-UI-1.4.3-Develop",
+        "build": "b999",
+        "date": "2017-08-14T19:35:00.000Z"
+    },
+    "kibana": {
+        "url": null, //if null then server.url will be used
+        "port": 5601,
+        "path": "/"
+    },
+    "authorization": {
+        "mode": "Basic",
+        "key": "cHJvZ25ldDpwcm9nbmV0"
+    }
+}
